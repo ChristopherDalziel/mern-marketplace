190d73def56d7214201c70354710e573b8fde0f1
Chris Dalziel <christopher_dalziel@hotmail.com>
2019-12-13 15:37:13 +1100

    Structuring the cart page, and components, backing up before cob

---
+++ components/Cart/CartItemList.js
@@ -0,0 +1,9 @@
+import  {Header, Segment, Button} from 'semantic-ui-react';
+
+// WHERE I'M CURRENTLY UP TO
+
+function CartItemList() {
+  return 
+}
+
+export default CartItemList;
---
+++ pages/cart.js
@@ -0,0 +1,14 @@
+import {Segment} from 'semantic-ui-react';
+
+import CartItemList from '../components/Cart/CartItemList';
+import CartSummary from '../components/Cart/CartSummary';
+
+function Cart() {
+  return 
+    <Segment>
+      <CartItemList />
+       <CartSummary />
+    </Segment>
+}
+
+export default Cart;
