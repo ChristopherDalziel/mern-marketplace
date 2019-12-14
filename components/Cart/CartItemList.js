09b6ae90a1154be6281d3a6c6a8b0504cc8039ba
Chris Dalziel <christopher_dalziel@hotmail.com>
2019-12-14 13:04:21 +1100

    cart page complete, moving onto creating users

--- components/Cart/CartItemList.js
+++ components/Cart/CartItemList.js
@@ -1,9 +1,26 @@
-import  {Header, Segment, Button} from 'semantic-ui-react';
-
-// WHERE I'M CURRENTLY UP TO
+import { Header, Segment, Button, Icon } from "semantic-ui-react";
 
 function CartItemList() {
-  return 
+  // Setting user signed in to false by default
+  const user = false;
+
+  return (
+    <Segment secondary color="black" inverted textAlign="center" placeholder>
+      <Header icon>
+        <Icon name="shopping basket" />
+        No products in your cart. Add some!
+      </Header>
+      <div>
+        {/* If user signed in show view products */}
+        {user ? (
+          <Button color="orange">View Products</Button>
+        ) : (
+          // If user is not signed in ask them to do so
+          <Button color="blue">Login to Add Products</Button>
+        )}
+      </div>
+    </Segment>
+  );
 }
 
 export default CartItemList;
--- pages/cart.js
+++ pages/cart.js
@@ -1,14 +1,14 @@
-import {Segment} from 'semantic-ui-react';
-
-import CartItemList from '../components/Cart/CartItemList';
-import CartSummary from '../components/Cart/CartSummary';
+import { Segment } from "semantic-ui-react";
+import CartItemList from "../components/Cart/CartItemList";
+import CartSummary from "../components/Cart/CartSummary";
 
 function Cart() {
-  return 
+  return (
     <Segment>
       <CartItemList />
-       <CartSummary />
+      <CartSummary />
     </Segment>
+  );
 }
 
 export default Cart;
