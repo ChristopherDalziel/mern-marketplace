2b951edb622d434cc352735a95a24271e570bddc
Chris Dalziel <christopher_dalziel@hotmail.com>
2019-12-17 18:20:15 +1100

    casscade delete to fix errors when removing things from the db

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,5 +1,7 @@
 import Product from '../../models/Product';
+import Cart from '../../models/Cart';
 import connectDb from "../../utils/connectDb";
+import Order from '../../models/Order';
 
 connectDb();
 
@@ -50,7 +52,28 @@ async function handlePostRequest(req, res) {
 
 async function handleDeleteRequest(req, res){
   const {_id} = req.query
-  // Using findOne to select everything not just one thing
-  await Product.findOneAndDelete({_id})
+  try {
+    // 1. Delete Product by ID
+    await Product.findOneAndDelete({_id})
+    // 2. Remove product from all carts, referenced as "product"
+    await Cart.updateMany(
+      {"products.product": _id },
+      // Pull product from the product array
+      {$pull: {products: {product: _id}}}
+    )
+    // 3. Remove product from "Order history" without this if you ever delete an item that is within a users history, the app will crash on the account page.
+    await Order.update(
+      {"products.product": _id },
+      // Pull product from the product array
+      {$pull: {products: {product: _id}}}
+    )
+
+    // Using findOne to select everything not just one thing
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error deleting product')
+  }
+
   res.status(204).json({})
 }
