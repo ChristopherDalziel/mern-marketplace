b099b9c278fb8c3587bd4bb8514a41a5ee6aa7c7
Chris Dalziel <christopher_dalziel@hotmail.com>
2019-12-19 10:05:55 +1100

    trying to fix timeout error on account page once hosted

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,12 +1,9 @@
-import User from "../../models/User";
+import User from '../../models/User';
 import jwt from "jsonwebtoken";
 
 export default async (req, res) => {
   try {
-    const { userId } = jwt.verify(
-      req.headers.authorization,
-      process.env.JWT_SECRET
-    );
+    const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
     // Display every user except for ourselves. $ne is the operator for not equal + sort Users by alphabetical.
     const users = await User.find({ _id: { $ne: userId } }).sort({
       role: "asc"
